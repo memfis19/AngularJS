e738d4461ad3b84f88a2ab3a8f5d80991317a120
memfis <Rodion.Surzhenko@activewindow.dk>
2016-09-18 16:55:13 +0300

    updated functionality.

--- assignment1/assignment1-starter-code/js/app.js
+++ assignment1/assignment1-starter-code/js/app.js
@@ -1,9 +1,41 @@
 (function () {
   'use strict';
-  angular.module('LunchCalculator', [])
-  .controller('LunchCalculatorController', function($scope) {
-    $scope.lunchList = "";
-    $scope.lunchListCount = 0;
-    $scope.message = "";
-  });
+  angular.module('LunchCheck', [])
+  .controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.lunchList = "";
+  $scope.message = "";
+  $scope.messageColor = "";
+
+  $scope.processLunchList = function () {
+    if ($scope.lunchList == "") {
+      $scope.message = "Please enter data first";
+      $scope.messageColor = getColor(0);
+      return;
+    }
+
+    var sizeOfLunch = calculateLunch($scope.lunchList);
+    $scope.messageColor = getColor(sizeOfLunch);
+
+    if (sizeOfLunch <= 0) $scope.message = "Please enter data first";
+    if (sizeOfLunch > 0 && sizeOfLunch <= 3) $scope.message = "Enjoy!";
+    if (sizeOfLunch > 3) $scope.message = "Too much!";
+  }
+
+  function calculateLunch(string) {
+    var listOfLunch = string.split(',');
+    return listOfLunch.length;
+  }
+
+  function getColor(sizeOfLunch) {
+    var colorValue = "green";
+    if (sizeOfLunch <= 0) colorValue = "red";
+    return {color: colorValue};
+  }
+
+}
+
 })()
